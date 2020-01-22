52b2411cb5253a23a53d18c2f5f800e1ba0d7fd9
Fredrik Lindén <fredrik.linden.johansson@hotmail.se>
2020-01-22 12:46:50 +0100

    added map
    added map

---
+++ Frontend/lenny-lunch/src/components/login/GoogleMap/map.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import {
+    withGoogleMap,
+    GoogleMap,
+    Marker,
+  } from "react-google-maps";
+  const MapWithAMarker = withGoogleMap(props =>
+    <GoogleMap
+      defaultZoom={15}
+      defaultCenter={{ lat: 57.7089, lng: 11.9746 }}
+    >
+      <Marker
+        position={{ lat: 57.7089, lng: 11.9746 }}
+      />
+    </GoogleMap>
+  );
+  export class Map extends React.Component {
+  constructor(props) { 
+  super(props); 
+  this.state = { };
+  }
+  
+  render (){
+    return(
+      <div>
+        <MapWithAMarker
+          containerElement={<div style={{ height: `400px`, width: `400px` }} />}
+          mapElement={<div style={{ height: `400px`, width: `400px`}} />}
+        />
+      </div>
+      )
+    }
+  }
